de454134c51eb6fa7eca0a972016892fef5b59ac
Saleh Nagat <snagat@e2r9p1.1337.ma>
2022-10-13 22:27:30 +0100

    XD map works very hapi

--- src/draw_with_pixels/draw.c
+++ src/draw_with_pixels/draw.c
@@ -28,37 +28,37 @@ void	draw_map_p(t_data *data, int init)
 	t_data	wall;
 
 	r = 0;
-	c = 0;	
+	c = 0;
+	// while(r < data->state->map_height)
+	// {
+	// 	c = 0;
+	// 	while(c < data->state->map_width)
+	// 	{
+	// 		if (data->state->map[r][c] == WALL)
+	// 			draw_square(data, c, r, 0x0074af51);
+	// 		else if (data->state->map[r][c] == EMPTY || data->state->map[r][c] == PLAYER)
+	// 			draw_square(data, c, r, 0x00e5dad5);
+	// 		c++;
+	// 	}
+	// 	r++;
+	// }
+	r = 0;
 	while(r < data->state->map_height)
+	{
+		c = 0;
+		while(c < data->state->map_width)
 		{
-			c = 0;
-			while(c < data->state->map_width)
-			{
-				if (data->state->map[r][c] == WALL)
-					draw_square(data, c, r, 0x0074af51);
-				else if (data->state->map[r][c] == EMPTY || data->state->map[r][c] == PLAYER)
-					draw_square(data, c, r, 0x00e5dad5);
-				c++;
-			}
-			r++;
+			if (data->state->map[r][c] == PLAYER && init == 1)
+				draw_player(data, c,r, 0x00ff0000);
+			else if (init == 0)
+				draw_p(data, 0x00ff0000);
+			c++;
 		}
-		r = 0;
-		while(r < data->state->map_height)
-		{
-			c = 0;
-			while(c < data->state->map_width)
-			{
-				if (data->state->map[r][c] == PLAYER && init == 1)
-					draw_player(data, c,r, 0x00ff0000);
-				else if (init == 0)
-					draw_p(data, 0x00ff0000);
-				c++;
-			}
-			r++;
-		}
-		draw_everything(data, data->state->map, &wall);
-		mlx_put_image_to_window(data->mlx, data->win, wall.img, 0, 0);
-		mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
-		mlx_destroy_image(data->mlx, wall.img);
-		mlx_destroy_image(data->mlx, data->img);
+		r++;
+	}
+	draw_everything(data, data->state->map, &wall);
+	mlx_put_image_to_window(data->mlx, data->win, wall.img, 0, 0);
+	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
+	mlx_destroy_image(data->mlx, wall.img);
+	mlx_destroy_image(data->mlx, data->img);
 }
--- src/draw_with_pixels/draw_walls.c
+++ src/draw_with_pixels/draw_walls.c
@@ -25,25 +25,14 @@ void	draw_walls(t_data *wall,t_player delta,t_data *data,t_ray *ray)
 			fill_pixel(wall, (ray->x) * x_w, ray->y ,0xe486f1);
 		else if (ray->y >= wall_info.top_y && ray->y <= wall_info.bottom_y)
 		{
-		
 			if (sin(data->player->an) > 0 && delta.zone == 1)
-			{
-				printf("%d happy \n", get_no_width(data));
 				ft_texture(wall, (ray->x) * x_w, ray->y, data, delta,  (ray->y - wall_info.top_y) * (get_no_height(data)/ wall_info.wall_h),get_no_width(data), get_no_add(data));
-			}
 			else if (sin(data->player->an) < 0 && delta.zone == 1)
-			{
 				ft_texture(wall, (ray->x) * x_w, ray->y, data, delta,  (ray->y - wall_info.top_y) * (get_so_height(data)/ wall_info.wall_h), get_so_width(data), get_so_add(data));
-			}
 			else if (cos(data->player->an) > 0)
-			{
 				ft_texture(wall, (ray->x) * x_w, ray->y, data, delta,  (ray->y - wall_info.top_y) * (get_ea_height(data) / wall_info.wall_h), get_ea_width(data),get_ea_add(data));
-			}
 			else if (cos(data->player->an) < 0)
-			{
 				ft_texture(wall, (ray->x) * x_w, ray->y, data, delta,  (ray->y - wall_info.top_y) * (get_we_height(data) / wall_info.wall_h), get_we_width(data),  get_we_add(data));
-			}
-
 		}
 		else if (ray->y > wall_info.bottom_y && ray->y < HEIGHT)
 			fill_pixel(wall, (ray->x) * x_w, ray->y, 0x8c321f);
