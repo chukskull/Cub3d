a784806d5ec6268fcad5f97af4f4bd1bcc59c6bf
Oussama Labrahmi <0ssama@protonmail.com>
2022-10-16 09:43:23 +0100

    fixed norminette in utils

--- src/utils/get_addr_img.c
+++ src/utils/get_addr_img.c
@@ -1,22 +1,33 @@
-#include "cub3d.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_addr_img.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 09:42:00 by olabrahm          #+#    #+#             */
+/*   Updated: 2022/10/16 09:42:01 by olabrahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "cub3d.h"
 
 unsigned int	*get_no_add(t_data *data)
 {
-	return(data->texture->addr_north);
+	return (data->texture->addr_north);
 }
 
 unsigned int	*get_so_add(t_data *data)
 {
-	return(data->texture->addr_south);
+	return (data->texture->addr_south);
 }
 
 unsigned int	*get_we_add(t_data *data)
 {
-	return(data->texture->addr_west);
+	return (data->texture->addr_west);
 }
 
 unsigned int	*get_ea_add(t_data *data)
 {
-	return(data->texture->addr_east);
+	return (data->texture->addr_east);
 }
--- src/utils/get_textures.c
+++ src/utils/get_textures.c
@@ -1,22 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_textures.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 09:41:24 by olabrahm          #+#    #+#             */
+/*   Updated: 2022/10/16 09:42:55 by olabrahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-
-char    *get_north(t_data *data)
+char	*get_north(t_data *data)
 {
-	return(data->state->north_texture);
+	return (data->state->north_texture);
 }
 
 char	*get_south(t_data *data)
 {
-	return(data->state->south_texture);
+	return (data->state->south_texture);
 }
 
 char	*get_east(t_data *data)
 {
-	return(data->state->east_texture);
+	return (data->state->east_texture);
 }
 
 char	*get_west(t_data *data)
 {
-	return(data->state->west_texture);
+	return (data->state->west_texture);
 }
