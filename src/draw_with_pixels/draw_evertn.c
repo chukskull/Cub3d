f38b8693073bcb3debfbfb9b9b604950bbc01564
Saleh Nagat <snagat@e1r8p7.1337.ma>
2022-10-16 19:46:59 +0100

    trash correction corrected xD

--- src/draw_with_pixels/draw_evertn.c
+++ src/draw_with_pixels/draw_evertn.c
@@ -6,7 +6,7 @@
 /*   By: snagat <snagat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 10:00:16 by snagat            #+#    #+#             */
-/*   Updated: 2022/10/16 11:08:38 by snagat           ###   ########.fr       */
+/*   Updated: 2022/10/16 19:44:35 by snagat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ void	draw_everything(t_data *data, int **map)
 	t_player	delta;
 	double		an_beg;
 
-	an_beg = data->player->an - (60 / 2 * DEGREE);
+	an_beg = data->player->an + (60 / 2 * DEGREE);
 	data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	data->addr = (unsigned int *)mlx_get_data_addr(\
 data->img, &data->usls, &data->usls, &data->usls);
-	ray.x = 0;
-	while (ray.x < 60)
+	ray.x = 60;
+	while (ray.x > 0)
 	{
-		ray.an = an_beg + (ray.x * DEGREE);
+		ray.an = an_beg - (ray.x * DEGREE);
 		if (ray.an > 2 * M_PI)
 			ray.an -= 2 * M_PI;
 		delta = find_wall(map, data, ray.an);
 		draw_walls(delta, data, &ray);
-		ray.x += 59.0 / WIDTH;
+		ray.x -= 59.0 / WIDTH;
 	}
 }
